6aeccacde972d8d2332e9504a9d85d0566fa09f4
Darren <hldaz77@gmail.com>
2023-07-05 21:08:21 +0100

    javascript finalised, layout complete

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
 "use strict";
+
+let audioElement;
+
+// Function to play the audio file
+function playAudio(file) {
+  // Create a new audio element if it doesn't exist
+  if (!audioElement) {
+    audioElement = new Audio();
+  }
+
+  // Set the source file and play the audio
+  audioElement.src = file;
+  audioElement.play();
+}
+
+// Function to stop the audio
+function stopAudio() {
+  if (audioElement) {
+    audioElement.pause();
+    audioElement.currentTime = 0;
+  }
+}
+// Function to play the audio file on hover
+function playAudioOnHover(file) {
+  // Create a new audio element if it doesn't exist
+  if (!audioElement) {
+    audioElement = new Audio();
+  }
+
+  // Set the source file and play the audio
+  audioElement.src = file;
+  audioElement.play();
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   // Get all the genre images
   const genreImages = document.querySelectorAll(".genre-image");
@@ -63,11 +97,30 @@ document.addEventListener("DOMContentLoaded", function () {
           });
         }, 500);
       }
+
       // Save the genre votes, decade votes, individual votes, and the person's name to local storage
       saveToLocalStorage();
     });
-  });
 
+    genreImages.forEach((image) => {
+      image.addEventListener("mouseenter", function () {
+        const name = document.getElementById("nameInput").value;
+        if (!name) {
+          return; // Do nothing if the name is not entered
+        }
+
+        const file = this.dataset.file;
+        if (file) {
+          playAudioOnHover(file);
+        }
+      });
+
+      // Add mouseleave event listeners to stop the audio on mouse leave
+      image.addEventListener("mouseleave", function () {
+        stopAudio();
+      });
+    });
+  });
   // Add click event listeners to each decade image
   decadeImages.forEach((image) => {
     image.addEventListener("click", function () {
@@ -139,6 +192,8 @@ const decadeVotes = votes.decadeVotes || {};
 // Generate a table for genre votes
 const genreTable = document.createElement("table");
 genreTable.innerHTML = "<tr><th>Genre</th><th>Votes</th></tr>";
+genreTable.style.fontFamily = "Bebas Neue";
+genreTable.style.fontSize = "2rem";
 
 for (const genre in genreVotes) {
   const row = document.createElement("tr");
@@ -186,6 +241,8 @@ new Chart(genreChartCanvas, {
 // Generate a table for decade votes
 const decadeTable = document.createElement("table");
 decadeTable.innerHTML = "<tr><th>Decade</th><th>Votes</th></tr>";
+genreTable.style.fontFamily = "Bebas Neue";
+genreTable.style.fontSize = "2rem";
 
 for (const decade in decadeVotes) {
   const row = document.createElement("tr");
@@ -233,20 +290,17 @@ new Chart(decadeChartCanvas, {
 // Create the refresh button element
 const refreshButton = document.createElement("button");
 refreshButton.id = "refreshButton";
-refreshButton.textContent = "Refresh";
+refreshButton.textContent = "Vote Again";
 
-// Append the refresh button to the appropriate container or element
-// For example:
-document.body.appendChild(refreshButton);
-
-// Attach a click event listener to the refresh button
-function attachRefreshButtonListener() {
-  document
-    .getElementById("refreshButton")
-    .addEventListener("click", function () {
-      location.reload();
-    });
+// Append the refresh button to the appropriate container or element on the voting page
+const votingPageContainer = document.getElementById("votingPageContainer");
+if (votingPageContainer) {
+  votingPageContainer.appendChild(refreshButton);
 }
 
+// Attach a click event listener to the refresh button
+refreshButton.addEventListener("click", function () {
+  location.href = location.href;
+});
 // Call the function to attach the event listener after the refresh button is created
 attachRefreshButtonListener();
--- testing.js
+++ testing.js
@@ -1,15 +1,36 @@
 "use strict";
 
-// GenreVote constructor function
-function GenreVote(name, genre) {
-  this.name = name;
-  this.genre = genre;
+let audioElement;
+
+// Function to play the audio file
+function playAudio(file) {
+  // Create a new audio element if it doesn't exist
+  if (!audioElement) {
+    audioElement = new Audio();
+  }
+
+  // Set the source file and play the audio
+  audioElement.src = file;
+  audioElement.play();
 }
 
-// DecadeVote constructor function
-function DecadeVote(name, decade) {
-  this.name = name;
-  this.decade = decade;
+// Function to stop the audio
+function stopAudio() {
+  if (audioElement) {
+    audioElement.pause();
+    audioElement.currentTime = 0;
+  }
+}
+// Function to play the audio file on hover
+function playAudioOnHover(file) {
+  // Create a new audio element if it doesn't exist
+  if (!audioElement) {
+    audioElement = new Audio();
+  }
+
+  // Set the source file and play the audio
+  audioElement.src = file;
+  audioElement.play();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -76,11 +97,30 @@ document.addEventListener("DOMContentLoaded", function () {
           });
         }, 500);
       }
+
       // Save the genre votes, decade votes, individual votes, and the person's name to local storage
       saveToLocalStorage();
     });
-  });
 
+    genreImages.forEach((image) => {
+      image.addEventListener("mouseenter", function () {
+        const name = document.getElementById("nameInput").value;
+        if (!name) {
+          return; // Do nothing if the name is not entered
+        }
+
+        const file = this.dataset.file;
+        if (file) {
+          playAudioOnHover(file);
+        }
+      });
+
+      // Add mouseleave event listeners to stop the audio on mouse leave
+      image.addEventListener("mouseleave", function () {
+        stopAudio();
+      });
+    });
+  });
   // Add click event listeners to each decade image
   decadeImages.forEach((image) => {
     image.addEventListener("click", function () {
@@ -93,7 +133,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       // Check if the image has already been clicked or if the limit has been reached
       if (this.classList.contains("clicked") || decadeClickCount >= 3) {
-        return;
+        return; // Do nothing if already clicked or limit reached
       }
 
       // Add 'clicked' class to the image
@@ -152,6 +192,8 @@ const decadeVotes = votes.decadeVotes || {};
 // Generate a table for genre votes
 const genreTable = document.createElement("table");
 genreTable.innerHTML = "<tr><th>Genre</th><th>Votes</th></tr>";
+genreTable.style.fontFamily = "Bebas Neue";
+genreTable.style.fontSize = "2rem";
 
 for (const genre in genreVotes) {
   const row = document.createElement("tr");
@@ -199,6 +241,8 @@ new Chart(genreChartCanvas, {
 // Generate a table for decade votes
 const decadeTable = document.createElement("table");
 decadeTable.innerHTML = "<tr><th>Decade</th><th>Votes</th></tr>";
+genreTable.style.fontFamily = "Bebas Neue";
+genreTable.style.fontSize = "2rem";
 
 for (const decade in decadeVotes) {
   const row = document.createElement("tr");
@@ -242,3 +286,21 @@ new Chart(decadeChartCanvas, {
     ],
   },
 });
+
+// Create the refresh button element
+const refreshButton = document.createElement("button");
+refreshButton.id = "refreshButton";
+refreshButton.textContent = "Vote Again";
+
+// Append the refresh button to the appropriate container or element on the voting page
+const votingPageContainer = document.getElementById("votingPageContainer");
+if (votingPageContainer) {
+  votingPageContainer.appendChild(refreshButton);
+}
+
+// Attach a click event listener to the refresh button
+refreshButton.addEventListener("click", function () {
+  location.href = location.href;
+});
+// Call the function to attach the event listener after the refresh button is created
+attachRefreshButtonListener();
